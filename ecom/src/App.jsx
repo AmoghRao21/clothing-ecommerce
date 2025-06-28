51bf298883d71f0e06bdfddcf11b08bda4962107
unknown <rao.amogh21@gmail.com>
2025-06-28 18:39:02 +0530

    Shop Page updated

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -8,6 +8,7 @@ import TopSellers from "./pages/Top";
 import FitFeed from "./pages/FitFeed";
 import DarkELKChatbot from "./components/Chatbot";
 import TShirtCustomizer from "./pages/Customization";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
@@ -27,7 +28,7 @@ function App() {
         />
         <Route path="/fitfeed" element={<FitFeed />} />
         <Route path="/customization" element={<TShirtCustomizer />} />
-
+        <Route path="/shop" element={<ShopPage />} />
         <Route
           path="*"
           element={<div className="text-white p-10">404 - Page Not Found</div>}
